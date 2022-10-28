e13b8b107c54cac7a172e0cdcc0325221b87726b
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-28 17:45:22 +0100

    Reconfigure TSC to allow JS files to be built

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const newsClient_1 = __importDefault(require("./newsClient"));
+const newsClient = new newsClient_1.default();
+newsClient.getAllHeadlines((data) => {
+    console.log(data);
+});
