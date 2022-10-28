ae002983b284a62934e05bdf377aefc1141a0f79
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-28 17:42:59 +0100

    Add .getAllHeadlines logic

--- src/newsClient.ts
+++ src/newsClient.ts
@@ -1,5 +1,10 @@
+import guardianApiKey from "./guardianApiKey"
+
 export default class NewsClient {
-  getAllHeadlines() {
-    fetch('')
+  getAllHeadlines(callBack: Function) {
+    fetch('https://content.guardianapis.com/search?api-key=' + guardianApiKey)
+      .then(res => { res.json() })
+      .then(data => callBack(data))
+      .catch(err => console.log('Could not fetch from Guardin API: ' + err.message))
   }
 }
