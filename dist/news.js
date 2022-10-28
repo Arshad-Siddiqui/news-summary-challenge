cc9feb6d6fcdfcc568e1bc9f26a3d154e90f8425
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-28 20:51:17 +0100

    Make News store relevant information

--- dist/news.js
+++ dist/news.js
@@ -2,6 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class News {
     constructor(data) {
+        this.id = data.id;
+        this.type = data.type;
+        this.sectionName = data.sectionName;
+        this.webPublicationDate = data.webPublicationDate;
+        this.webTitle = data.webTitle;
+        this.webUrl = data.webUrl;
+        this.apiUrl = data.apiUrl;
     }
 }
 exports.default = News;
--- src/news.ts
+++ src/news.ts
@@ -1,8 +1,21 @@
 import { NewsData } from "./newsModel";
 
 export default class News {
-
+  id: string;
+  type: string;
+  sectionName: string;
+  webPublicationDate: string;
+  webTitle: string;
+  webUrl: string;
+  apiUrl: string;
   constructor(data: NewsData) {
-    
+    this.id = data.id;
+    this.type = data.type;
+    this.sectionName = data.sectionName;
+    this.webPublicationDate = data.webPublicationDate;
+    this.webTitle = data.webTitle;
+    this.webUrl = data.webUrl;
+    this.apiUrl = data.apiUrl;
   }
 }
+
