7aaef0d8ee18ab655464196e8187ae9d4971472b
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-29 19:21:31 +0100

    Improve positioning of header elements

---
+++ dist/searchClient.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class searchClient {
+    searchApi(query) {
+    }
+}
+exports.default = searchClient;
---
+++ src/searchClient.ts
@@ -0,0 +1,5 @@
+export default class searchClient {
+  searchApi(query: string) {
+    
+  }
+}
